aa45635118abac0b5e568ecc81e8dd00c4a8afd5
Tetsutaka <d611030k@gmail.com>
2023-05-08 22:45:07 +0900

    Update to display an image

--- clock.py
+++ clock.py
@@ -1,12 +1,28 @@
 import tkinter as tk
+from PIL import Image, ImageTk
 
 # ウィンドウを作る
 root = tk.Tk()
 
-# ウィンドウのサイズの設定
-root.geometry("640x360")
+# ウィンドウの最大化
+root.state("zoomed")  # Windowsでも動作可能
+# root.attributes("-fullscreen", True)  # Mac, Linuxでのみ動作可能
+
 # ウィンドウのタイトルの設定
 root.title("clock app")
+root.configure(bg="lightblue")
+
+
+
+# 画像の読み込み
+img_path = "./data/sample.jpeg"
+img = Image.open(img_path)
+# tkinterで扱える画像形式に変換
+bg_img = ImageTk.PhotoImage(img)
+# 画像をラベルとして設定
+label = tk.Label(root, image=bg_img)
+# 画像ラベルを配置
+label.pack(fill=tk.BOTH, expand=True)
 
 # ウィンドウの表示
 root.mainloop()
